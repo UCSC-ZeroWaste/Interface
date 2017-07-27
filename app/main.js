f9fce879b4db520b96665d419b1963775fb04b72
Nelson Oewel <nelson.oewel@gmail.com>
2017-07-27 16:22:15 -0700

    installed dependencies for different loaders, added loaders to webpack.config.js, imported bootstrap.css in main.js

--- app/main.js
+++ app/main.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
+import Bootstrap from './bootstrap/dist/css/bootstrap.css'
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -42,6 +42,24 @@ module.exports = {
     }, {
       test: /\.css$/,
       loader: 'style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]'
+    },{
+      test: /\.png$/, 
+      loader: "url-loader?limit=100000" 
+    },{
+      test: /\.jpg$/, 
+      loader: "file-loader"
+    },{
+      test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
+      loader: 'url?limit=10000&mimetype=application/font-woff'
+    },{
+      test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
+      loader: 'url?limit=10000&mimetype=application/octet-stream'
+    },{
+      test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
+      loader: 'file'
+    },{
+      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
+      loader: 'url?limit=10000&mimetype=image/svg+xml'
     }]
   }
 };
