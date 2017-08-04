6cdae08abb3b58c9d35d29aa6822d8f4bc56d616
DeliSauce <nycdelisauce@gmail.com>
2017-08-03 23:17:45 -0700

    Add font awesome icons to nav bar buttons.

--- app/lib/components/navbar/NavButton.jsx
+++ app/lib/components/navbar/NavButton.jsx
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {handleNavSelect} from '../../actions/views';
 import styles from '../../../App.css';
-
+import FontAwesome from 'react-fontawesome';
 
 class NavButton extends Component {
   constructor(props) {
@@ -16,12 +16,19 @@ class NavButton extends Component {
       <button
         className={styleButton}
         onClick={() => this.props.handleNavSelect(this.props.view)}>
-        {this.props.name}
+          <FontAwesome
+            name={this.props.fa}
+            size='3x'
+            style={{ width: '50', height: '50' }}
+            />
       </button>
     );
   }
 }
-
+// {this.props.name}
+// <div style={{width: '50', height: '50', backgroundColor: 'blue'}}>
+// </div>
+// style={{ width: '50', height: '50', textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
 
 const mapStateToProps = (state, ownProps) => ({
   currentView: state.currentView,
