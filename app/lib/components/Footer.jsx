c90025114409e938f7e5dd2f30b6c64f5de8cc5c
Katie Patrick <katiepatrickgp@gmail.com>
2017-08-21 17:05:53 -0700

    A Little CSS

--- app/lib/components/Footer.jsx
+++ app/lib/components/Footer.jsx
@@ -12,9 +12,9 @@ export default class NavBar extends Component {
     return (
       <div className={styles.footer_container}>
           <div className={styles.footer}>
-            <SiteSelector/>
             <NavButton title={'Local'} nav={'scope'} scope={'local'} fa={'user'}/>
             <NavButton title={'Global'} nav={'scope'} scope={'global'} fa={'globe'}/>
+            <SiteSelector/>
           </div>
       </div>
     );
