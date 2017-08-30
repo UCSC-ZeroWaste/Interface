9e26b5124fc10a03d1a062f84defcb8a7f9e3d6a
DeliSauce <nycdelisauce@gmail.com>
2017-08-30 14:35:57 -0700

    Add slug images to markers.

--- app/lib/components/views/HeatMap.jsx
+++ app/lib/components/views/HeatMap.jsx
@@ -3,11 +3,12 @@ import {mapsStaticKey, mapsJavascriptKey} from '../../../config.js';
 import styles from '../../../App.css';
 import GoogleMapReact from 'google-map-react';
 import {SLUG_PINS} from '../../constants/constants';
+import {connect} from 'react-redux';
 
 // const map1 = 'https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=' + settings.key;
 
 
-export default class HeatMap extends Component {
+class HeatMap extends Component {
   constructor(props) {
     super(props);
   }
@@ -53,27 +54,29 @@ export default class HeatMap extends Component {
 
 
     renderMarkers() {
+      const MARKERS = {
+        'Kresge': {lat: '36.9972381', long: '-122.0667945'},
+        'Oakes': {lat: '36.9890294', long: '-122.0646362'},
+        'Porter': {lat: '36.9943943', long: '-122.0652214'},
+        'College Eight': {lat: '36.9911913', long: '-122.0647242'},
+        'College Nine and Ten': {lat: '36.9911913', long: '-122.0647242'},
+        'Cowell-Stevenson': {lat: '36.9911913', long: '-122.0647242'},
+        'Crown-Merrill': {lat: '36.9911913', long: '-122.0647242'},
+      };
 
-      // "College Nine and Ten"
-      // "Cowell-Stevenson"
-      // "Crown-Merrill"
-      const MARKERS = [
-        {name: 'Kresge', lat: '36.9972381', long: '-122.0667945'},
-        {name: 'Oakes', lat: '36.9890294', long: '-122.0646362'},
-        {name: 'Porter', lat: '36.9943943', long: '-122.0652214'},
-        {name: 'College Eight', lat: '36.9911913', long: '-122.0647242'}
-      ];
+      return this.props.leaders.map( (leader, index) => {
+        let options = MARKERS[leader.site]
+        let slugImage = SLUG_PINS[index];
 
-      return MARKERS.map( (site, index) => {
         return (
           <div
-            lat={site.lat}
-            lng={site.long}
+            lat={options.lat}
+            lng={options.long}
             key={index}
             style={{
               height: '70px',
               width: '70px',
-              backgroundImage: `url(${SLUG_PINS[4]})`,
+              backgroundImage: `url(${slugImage})`,
               backgroundSize: 'contain',
             }}
             ></div>
@@ -122,3 +125,10 @@ export default class HeatMap extends Component {
      );
   }
 }
+
+
+const mapStateToProps = (state) => ({
+  leaders: state.records.leaders
+})
+
+export default connect(mapStateToProps)(HeatMap);
