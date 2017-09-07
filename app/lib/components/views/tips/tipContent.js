196f7c94e19e7506a5f0744e679c90c5cb66fe09
Katie Patrick <katiepatrickgp@gmail.com>
2017-09-06 19:49:23 -0700

    Added content for actions

--- app/lib/components/views/tips/tipContent.js
+++ app/lib/components/views/tips/tipContent.js
@@ -1,19 +1,68 @@
 import image1 from "../../../assets/images/tips/corgy_image.jpg";
+import image2 from "../../../assets/images/tips/corgy_image.jpg";
+import image3 from "../../../assets/images/tips/corgy_image.jpg";
+import image4 from "../../../assets/images/tips/corgy_image.jpg";
+import image5 from "../../../assets/images/tips/corgy_image.jpg";
+import image6 from "../../../assets/images/tips/corgy_image.jpg";
+import image7 from "../../../assets/images/tips/corgy_image.jpg";
+import image8 from "../../../assets/images/tips/corgy_image.jpg";
+import image9 from "../../../assets/images/tips/corgy_image.jpg";
+import image10 from "../../../assets/images/tips/corgy_image.jpg";
 
 export default [
   {
-    title: "Buy food in bulk",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    title: "Fall in love with your reusable water bottle",
+    text: "Single use plastics are one of your greatest enemies! One of the easiest practices to adopt is to replace plastic bottled drinks, especially bottled water, with your own beloved reusable bottle. Get yourself a beautiful high quality reusable bottle and fill it up with water and take it with you everytime you leave the house. Next time you leave the house, check “Keys, phone, wallet, WATER BOTTLE!",
     image: image1
   },
   {
-    title: "This and that.",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    image: image1
+    title: "Take your reusable cup to the coffee shop",
+    text: "Get yourself a reusable coffee mug and quit accepting disposable paper cups at coffee shops. Reusable mugs are superior. They keep the heat in and they seal properly so you don’t need to worry about spills or leaks.  They can last indefinitely and many coffee shops offer a discount on your beverage when you use it!",
+    image: image2
   },
   {
-    title: "The other.",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    image: image1
+    title: "Compost for the climate",
+    text: "Did you know that food waste makes up the largest portion of landfill? Organic matter (like food waste) releases methane gas when it is stored in low oxygen environments like landfills. Eighteen percent of America’s methane emissions come from landfills.12 This is bad because methane contributes 28 percent of the global warming we are experiencing and the amount of methane in the atmosphere has doubled in the last 100 years.12 This means that composting is a serious issue for climate change! When food waste is allowed to naturally decompose in an open air compost, doesn’t release methane and turns into nutrient dense, carbon rich soil. In addition to preventing methane production, composting will capture all those minerals and carbon that get absorbed from the soil by crops, and return it back into the soil. Think of food scraps like a multivitamin for soil. The last place we want compost is in the landfill!",
+    image: image3
+  },
+    {
+    title: "Take a container for your take out",
+    text: "Next time you go out to eat, or order take-out, bring your own container to the restaurant and ask the food server to kindly put the food in your own container. Try to ‘eat in’ at restaurants whenever you can if you don’t have a container with you in order to avoid the dreaded excess take-out packaging. Be sure to use your own containers to take home your leftovers too. It only feels weird to ask the server the first time you do it!",
+    image: image4
+  },
+  {
+    title: "Quit plastic straws and use bamboo, glass, or stainless steel straws",
+    text: "Bamboo straws are cool! They have an rustic asian look and such a novelty will impress any guests. The large diameter makes them easy to clean. Bamboo is durable and your straws will last. You might also like glass or stainless steel straws as well. Be careful to refuse plastic straws at restaurants and bars.",
+    image: image4
+  },
+   {
+    title: "Use a shampoo bar",
+    text: "Shampoo doesn’t have to come in plastic bottles. You can buy shampoo as a solid bar and avoid the plastic altogether. There is a smorgasbord of these bars available and they are not expensive at all. The ingredients in shampoo bars are really simple: olive oil, coconut oil, castor oil, sodium hydroxide, and pure essential oils for fragrance.",
+    image: image5
+  },
+   {
+    title: "Donate or resell old clothes",
+    text: "Give your old clothes away to people, not landfills. To make sure that your old clothes never hit the landfill, donate or resell them. Find your local Goodwill bin and never let a textile hit the trash again. You can always sell your own high quality clothes online too. There are many cool web apps specialising in reselling of pre-loved clothes.",
+    image: image6
+  },
+    {
+    title: "Buy bamboo toothbrushes",
+    text: "There is no need for plastic toothbrushes. We can get them made out of bamboo! Bamboo toothbrushes are made out of 100% compostable bamboo fibres - even the bristles!  They work just as well as plastic toothbrushes, but they do wear out a little sooner. You can get them from Wholefoods or purchase them online. But make sure you actually do compost them after they have worn out!",
+    image: image7
+  },
+   {
+    title: "Make fun, not stuff",
+    text: "This is the art of life! Build your life around pleasurable activities that do not require purchasing new items or eating unhealthy packaged food. Choose fun things to do like hiking, singing, playing a musical instrument, gardening, yoga, dance class, writing romantic poetry, or seeing a live music show. Avoid going shopping at malls or just buying things for fun. Websites like Groupon, Livingsocial, and Meetup.com have activities in your area (like kayaking or massage) that you might not usually think of doing.",
+    image: image8
+  },
+   {
+    title: "Use handkerchiefs instead of tissues",
+    text: "Don’t be afraid of the handkerchief! Vintage handkerchiefs come beautifully designed and embroidered. The handkerchief is making a vintage hipster resurgence. Once you’ve crossed the line to the stylish hanky, you won’t go back! Just throw them in the wash every time you do laundry.",
+    image: image9
+  },
+    {
+    title: "Use cloth instead of paper towels",
+    text: "Paper towels are so common that we may not question if we really need them. It’s quite easy to quit using cloth towels in the kitchen. Keep a box or jar of cloth squares that can be used to replace paper towels. Keep another container to put the used towels in so that everything is nicely organised.",
+    image: image10
   }
 ];
