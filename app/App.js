8e6d9ccf04a1edd7f5e76768b4ee6ce9e39b4975
DeliSauce <nycdelisauce@gmail.com>
2017-09-13 17:30:38 -0700

    Create Landing Page and enable site selection.

--- app/App.js
+++ app/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import styles from './App.css';
 import Carousel from './lib/components/Carousel.jsx';
+import LandingPage from './lib/components/LandingPage.jsx';
 
 import { Provider } from 'react-redux';
 import {fetchRecords, fetchDummyRecords} from './lib/actions/record_actions';
 
 import {connect} from 'react-redux';
 import _ from 'underscore';
-import {HashRouter, Route} from 'react-router-dom';
+import {HashRouter, Route, Switch} from 'react-router-dom';
 
 // import TestTransitions from './lib/components/test_carousel/TestTransitions';
 // import TestRouter from './lib/components/test_carousel/TestRouter';
@@ -47,8 +48,11 @@ class App extends React.Component {
 
     return (
       <Provider store={this.props.store}>
-        <HashRouter basename="carousel">
-          <Route path="/site/:siteIndex" component={Carousel} />
+        <HashRouter>
+          <Switch>
+            <Route path="/carousel/site/:siteIndex" component={Carousel} />
+            <Route path="/home" component={LandingPage} />
+          </Switch>
         </HashRouter>
       </Provider>
     );
---
+++ app/lib/components/LandingPage.jsx
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import styles from '../../App.css';
+import NavBar from './NavBar.jsx';
+import Footer from './Footer.jsx';
+import {Link, Redirect, withRouter} from 'react-router-dom';
+import {COLLEGE_SET} from '../constants/constants';
+
+const SiteButton = ({siteName, goToCarousel}) => {
+  return (
+    <div
+      className={styles.landing_page_site_button}
+      onClick={goToCarousel}
+      >
+      {siteName}
+    </div>
+  );
+};
+
+export default class LandingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      siteSelectionHidden: true
+    };
+    this.handleCampusSelect = this.handleCampusSelect.bind(this);
+    this.toggleSiteSelection = this.toggleSiteSelection.bind(this);
+  }
+
+  handleCampusSelect() {
+  }
+
+  toggleSiteSelection() {
+    this.setState({siteSelectionHidden: !this.state.siteSelectionHidden});
+  }
+
+  renderSiteSelectionContainer() {
+    return (
+      <div className={styles.landing_page_site_selection_container}>
+        {COLLEGE_SET.map( (siteName, i) => {
+          return (
+            <SiteButton
+              key={i}
+              siteName={siteName}
+              goToCarousel={() => this.props.history.push(`/carousel/site/${i}`)}
+              />
+          );
+        })}
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className={styles.page}>
+        <NavBar />
+        <div className={styles.main_view}>
+          <div className={styles.landing_container}>
+            <div className={styles.landing_text}>
+              We're reaching zero waste...<br/> and having fun doing it.
+            </div>
+            <div
+              className={styles.landing_page_button}
+              onClick={this.toggleSiteSelection}>
+              Choose Your House
+            </div>
+            <div
+              className={styles.landing_page_button}
+              onClick={this.handleCampusSelect}>
+              See Whole Campus
+            </div>
+            {this.state.siteSelectionHidden
+              ?
+              null
+              : this.renderSiteSelectionContainer()
+            }
+          </div>
+        </div>
+        <Footer/>
+      </div>
+    );
+  }
+}
