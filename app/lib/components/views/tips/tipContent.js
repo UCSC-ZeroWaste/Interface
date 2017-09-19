fddf882bb05e2b81a2bd34fc35136fbdbf3ab606
Katie Patrick <katiepatrickgp@gmail.com>
2017-09-19 15:12:30 -0700

    Draft images

--- app/lib/components/views/tips/tipContent.js
+++ app/lib/components/views/tips/tipContent.js
@@ -1,13 +1,14 @@
-import image1 from "../../../assets/images/tips/corgy_image.jpg";
-import image2 from "../../../assets/images/tips/corgy_image.jpg";
-import image3 from "../../../assets/images/tips/corgy_image.jpg";
-import image4 from "../../../assets/images/tips/corgy_image.jpg";
-import image5 from "../../../assets/images/tips/corgy_image.jpg";
-import image6 from "../../../assets/images/tips/corgy_image.jpg";
-import image7 from "../../../assets/images/tips/corgy_image.jpg";
-import image8 from "../../../assets/images/tips/corgy_image.jpg";
-import image9 from "../../../assets/images/tips/corgy_image.jpg";
-import image10 from "../../../assets/images/tips/corgy_image.jpg";
+import image1 from "../../../assets/images/tips/bottle.jpg";
+import image2 from "../../../assets/images/tips/coffee.jpg";
+import image3 from "../../../assets/images/tips/compost.jpg";
+import image4 from "../../../assets/images/tips/container.jpg";
+import image5 from "../../../assets/images/tips/straws.jpg";
+import image6 from "../../../assets/images/tips/shampoo_bar.jpg";
+import image7 from "../../../assets/images/tips/cloths.jpg";
+import image8 from "../../../assets/images/tips/bamboo_toothbrush.jpg";
+import image9 from "../../../assets/images/tips/fun.jpg";
+import image10 from "../../../assets/images/tips/handkerchief.jpg";
+import image11 from "../../../assets/images/tips/cloth.jpg";
 
 export default [
   {
@@ -33,36 +34,36 @@ export default [
   {
     title: "Quit plastic straws and use bamboo, glass, or stainless steel straws",
     text: "Bamboo straws are cool! They have an rustic asian look and such a novelty will impress any guests. The large diameter makes them easy to clean. Bamboo is durable and your straws will last. You might also like glass or stainless steel straws as well. Be careful to refuse plastic straws at restaurants and bars.",
-    image: image4
+    image: image5
   },
    {
     title: "Use a shampoo bar",
     text: "Shampoo doesn’t have to come in plastic bottles. You can buy shampoo as a solid bar and avoid the plastic altogether. There is a smorgasbord of these bars available and they are not expensive at all. The ingredients in shampoo bars are really simple: olive oil, coconut oil, castor oil, sodium hydroxide, and pure essential oils for fragrance.",
-    image: image5
+    image: image6
   },
    {
     title: "Donate or resell old clothes",
     text: "Give your old clothes away to people, not landfills. To make sure that your old clothes never hit the landfill, donate or resell them. Find your local Goodwill bin and never let a textile hit the trash again. You can always sell your own high quality clothes online too. There are many cool web apps specialising in reselling of pre-loved clothes.",
-    image: image6
+    image: image7
   },
     {
     title: "Buy bamboo toothbrushes",
     text: "There is no need for plastic toothbrushes. We can get them made out of bamboo! Bamboo toothbrushes are made out of 100% compostable bamboo fibres - even the bristles!  They work just as well as plastic toothbrushes, but they do wear out a little sooner. You can get them from Wholefoods or purchase them online. But make sure you actually do compost them after they have worn out!",
-    image: image7
+    image: image8
   },
    {
     title: "Make fun, not stuff",
     text: "This is the art of life! Build your life around pleasurable activities that do not require purchasing new items or eating unhealthy packaged food. Choose fun things to do like hiking, singing, playing a musical instrument, gardening, yoga, dance class, writing romantic poetry, or seeing a live music show. Avoid going shopping at malls or just buying things for fun. Websites like Groupon, Livingsocial, and Meetup.com have activities in your area (like kayaking or massage) that you might not usually think of doing.",
-    image: image8
+    image: image9
   },
    {
     title: "Use handkerchiefs instead of tissues",
     text: "Don’t be afraid of the handkerchief! Vintage handkerchiefs come beautifully designed and embroidered. The handkerchief is making a vintage hipster resurgence. Once you’ve crossed the line to the stylish hanky, you won’t go back! Just throw them in the wash every time you do laundry.",
-    image: image9
+    image: image10
   },
     {
     title: "Use cloth instead of paper towels",
     text: "Paper towels are so common that we may not question if we really need them. It’s quite easy to quit using cloth towels in the kitchen. Keep a box or jar of cloth squares that can be used to replace paper towels. Keep another container to put the used towels in so that everything is nicely organised.",
-    image: image10
+    image: image11
   }
 ];
